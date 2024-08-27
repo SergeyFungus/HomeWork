129c60563a43ab03ab34879c128635a76f3f7917
SergeyFungus <dudin570@mail.ru>
2024-08-27 18:55:29 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task =  hours_spent / homework_count
+print('Курс: ' , course_name , ', всего задач: ', homework_count, ', затрачено часов: ', hours_spent, ', среднее время выполнения ', time_for_one_task, 'часа' )
