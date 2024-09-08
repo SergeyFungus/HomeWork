197533e8f79a3a744620583bc58b89076dce3c8d
SergeyFungus <dudin570@mail.ru>
2024-09-08 14:25:33 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -3,14 +3,15 @@ primes = []
 not_primes = []
 for i in numbers:
     is_prime = True
+    if i == 1:
+        continue
     for j in range(2, i):
-        if i % j == 0 or i == 1:
+        if i % j == 0:
            is_prime = False
            break
-    if i != 1:
-        if is_prime:
-            primes.append(i)
-        else:
-            not_primes.append(i)
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
 print(primes)
 print(not_primes)
