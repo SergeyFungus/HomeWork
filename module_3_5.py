cb18bdc6a3a4bdcb0dc0b3eca1eaed85ea003dc0
SergeyFungus <dudin570@mail.ru>
2024-10-27 20:09:25 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(str_number[1:])
+    else:
+        return first
+
+
+result = get_multiplied_digits(40203) # Функция возвращает 0 в данном случае т.к. требовалось
+print(result)                         # вычислить произведение всех цифр числа, а 0 тоже
